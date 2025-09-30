d016cc2b6e6172c0af29a727c86bdc88537b3661
Jakub Jarosz <jakubjarosz2015@gmail.com>
2025-09-30 17:03:55 +0200

    Random_Commit

---
+++ src/test/java/com/kodilla/random_projects/RandomProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.random_projects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomProjectsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
