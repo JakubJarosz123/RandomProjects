f039a1724dc3600bca90aabe7318ab08f4985cbc
Jakub Jarosz <jakubjarosz2015@gmail.com>
2025-09-30 17:19:56 +0200

    Book and Order Completed1

--- src/main/java/Book.java
+++ src/main/java/Book.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
+import java.util.Objects;
 
 public class Book {
+    private String title;
+    private String author;
+    private int year;
+
+    public Book(String title) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                '}';
+    }
 }
--- src/main/java/Order.java
+++ src/main/java/Order.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.time.LocalDate;
 
 public class Order {
+    Book book;
+    LocalDate dateOfOrder;
+
+    public Order(Book book, int year, int month, int day) {
+        this.book = book;
+        this.dateOfOrder = LocalDate.of(year, month, day);
+    }
+
+    public Book getBook() {
+        return book;
+    }
+
+    public LocalDate getDateOfOrder() {
+        return dateOfOrder;
+    }
+
+    @Override
+    public String toString() {
+        return "Order: /n" + "book: " + book + "/n dateOfOrder=" + dateOfOrder;
+    }
 }
